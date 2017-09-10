757abad82b4a70238c6cc08b9bfc183b39871a22
svopportunity <31586871+svopportunity@users.noreply.github.com>
2017-09-10 10:23:18 +0800

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -10,7 +10,7 @@
 
 	//jQuery to collapse the navbar on scroll
 	$(window).scroll(function() {
-		if ($(".navbar").offset().top > 50 && $(".navbar").offset().width > 480) {
+		if ($(".navbar").offset().top > 50) {
 			$(".navbar-fixed-top").addClass("top-nav-collapse");
 		} else {
 			$(".navbar-fixed-top").removeClass("top-nav-collapse");
